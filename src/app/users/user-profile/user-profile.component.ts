bf157435773791c479be9d8251799e5659531253
Srashti Jain <srashti0204@gmail.com>
2022-10-11 00:25:10 +0530

    added model and form

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Form } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-  constructor() {}
+  userObj!: User;
+  constructor() {
+    this.userObj = {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+  }
 
   ngOnInit(): void {}
 
-  save(user: any) {
-    console.log(user);
+  save(userData: Form) {
+    console.log(userData);
   }
 }
